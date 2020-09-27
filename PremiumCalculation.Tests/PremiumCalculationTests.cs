3cb6e064807a909e2914aa5b94f2a7379e781316
Ian Baker <inbox@ianbaker.me>
2020-09-27 16:11:56 +0100

    Add project files.

---
+++ PremiumCalculation.Tests/PremiumCalculationTests.cs
@@ -0,0 +1,99 @@
+﻿
+using FluentAssertions;
+using Xunit;
+
+namespace PremiumCalculation.Tests
+{
+    public class PremiumCalculationTests
+    {
+        [Fact]
+        public void CheckIncludingCalculations()
+        {
+            // Arrange
+            var premiumExclIptAmount = 100;
+            var iptPercent = 12;
+
+            // Act
+            var sut = new Premium(iptPercent)
+            {
+                PremiumExclIptAmount = premiumExclIptAmount
+            };
+
+            // Assert
+            sut.PremiumInclIptAmount.Should().Be(112);
+            sut.PremiumExclIptAmount.Should().Be(premiumExclIptAmount);
+            sut.IptAmount.Should().Be(premiumExclIptAmount * (iptPercent / 100m));
+        }
+
+        [Fact]
+        public void CheckExcludingCalculations()
+        {
+            // Arrange
+            var premiumInclIptAmount = 112;
+            var iptPercent = 12;
+
+            // Act
+            var sut = new Premium(iptPercent)
+            {
+                PremiumInclIptAmount = premiumInclIptAmount
+            };
+
+            // Assert
+            sut.PremiumExclIptAmount.Should().Be(100);
+            sut.PremiumInclIptAmount.Should().Be(premiumInclIptAmount);
+            sut.IptAmount.Should().Be(100m * (iptPercent / 100m));
+        }
+
+        [Fact]
+        public void CheckIfPremiumOverridden_ThenCorrectValuesReturned()
+        {
+            // Arrange
+            var premiumInclIptAmount = 112m;
+            var overridePremium = 110m;
+            var iptPercent = 12m;
+            var expectedPremiumExclIptAmount = overridePremium / (1 + (iptPercent / 100m));
+
+            // Act
+            var sut = new Premium(iptPercent)
+            {
+                PremiumInclIptAmount = premiumInclIptAmount,
+                OverridePremium = overridePremium
+            };
+
+            // Assert
+            sut.PremiumInclIptAmount.Should().Be(110);
+            sut.PremiumExclIptAmount.Should().Be(expectedPremiumExclIptAmount);
+            sut.OverridePremium.Should().Be(overridePremium);
+            sut.IptAmount.Should().Be(overridePremium - expectedPremiumExclIptAmount);
+        }
+
+        [Theory]
+        [InlineData(12.0, 15.0, 100.0, 105.0, 12.0)]
+        [InlineData(12.0, 15.0, 1235.66, 1250.0, 12.0)]
+        [InlineData(12.0, 15.0, 1235.66, 1250.0, 185.349)]
+        public void CheckIfCommissionOverridden_ThenCorrectValuesReturned(decimal iptPercent, decimal commissionPercent, decimal premiumExclIptAmount, decimal overridePremium, decimal overrideCommission)
+        {
+            // Arrange
+            var maxCommission = premiumExclIptAmount * (commissionPercent / 100);
+            var commissionSacrificed = maxCommission - overrideCommission;
+            var expectedPremiumExclIptAmount = (overridePremium / (1 + (iptPercent / 100))) - commissionSacrificed;
+            var expectedIptAmount = expectedPremiumExclIptAmount * (iptPercent / 100);
+            var expectedPremiumInclIptAmount = expectedPremiumExclIptAmount + expectedIptAmount;
+
+            // Act
+            var sut = new Premium(iptPercent, commissionPercent)
+            {
+                PremiumExclIptAmount = premiumExclIptAmount,
+                OverridePremium = overridePremium,
+                OverrideCommission = overrideCommission
+            };
+
+            // Assert
+            sut.PremiumExclIptAmount.Should().Be(expectedPremiumExclIptAmount);
+            sut.PremiumInclIptAmount.Should().Be(expectedPremiumInclIptAmount);
+            sut.OverridePremium.Should().Be(overridePremium);
+            sut.IptAmount.Should().Be(expectedIptAmount);
+            sut.MaxCommission.Should().Be(premiumExclIptAmount * (commissionPercent / 100));
+        }
+    }
+}
---
+++ PremiumCalculation/Premium.cs
@@ -0,0 +1,88 @@
+﻿namespace PremiumCalculation
+{
+    public class Premium
+    {
+        private decimal premiumExclIptAmount;
+        private decimal premiumInclIptAmount;
+        private decimal overriddenPremiumExclIptAmount;
+        private decimal overriddenPremiumInclIptAmount;
+        private decimal overridePremium = 0;
+        private decimal overrideCommission = 0;
+
+        public Premium(decimal iptPercent, decimal commissionPercent = 0)
+        {
+            IptPercent = iptPercent;
+            CommissionPercent = commissionPercent;
+        }
+
+        public decimal PremiumExclIptAmount {
+            get
+            {
+                return overriddenPremiumExclIptAmount != 0 ? overriddenPremiumExclIptAmount : premiumExclIptAmount;
+            }
+            set 
+            {
+                premiumExclIptAmount = value;
+                IptAmount = premiumExclIptAmount * (IptPercent / 100);
+                premiumInclIptAmount = premiumExclIptAmount + IptAmount;
+            } 
+        }
+
+        public decimal PremiumInclIptAmount {
+            get
+            {
+                return overriddenPremiumInclIptAmount != 0 ? overriddenPremiumInclIptAmount : premiumInclIptAmount;
+            }
+            set {
+                premiumInclIptAmount = value;
+                premiumExclIptAmount = premiumInclIptAmount / (1 + (IptPercent / 100));
+                premiumExclIptAmount -= CommissionSacrificed;
+                IptAmount = premiumInclIptAmount - premiumExclIptAmount;
+            }
+        }
+
+        public decimal IptPercent { get; }
+        
+        public decimal CommissionPercent { get; }
+
+        public decimal IptAmount { get; private set; }
+
+        public decimal MaxCommission
+        {
+            get => premiumExclIptAmount * (CommissionPercent / 100);
+        }
+
+        public decimal OverridePremium
+        {
+            get => overridePremium;
+            set
+            {
+                overridePremium = value;
+                overriddenPremiumInclIptAmount = value;
+                overriddenPremiumExclIptAmount = overriddenPremiumInclIptAmount / (1 + (IptPercent / 100));
+                overriddenPremiumExclIptAmount -= CommissionSacrificed;
+                IptAmount = overriddenPremiumInclIptAmount - overriddenPremiumExclIptAmount;
+            }
+        }
+
+        public decimal OverrideCommission
+        {
+            get => OverrideCommission;
+            set
+            {
+                overrideCommission = value;
+                overriddenPremiumExclIptAmount -= CommissionSacrificed;
+                IptAmount = overriddenPremiumExclIptAmount * (IptPercent / 100);
+                overriddenPremiumInclIptAmount = overriddenPremiumExclIptAmount + IptAmount;
+            }
+        }
+
+        private decimal CommissionSacrificed
+        {
+            get
+            {
+                return overrideCommission == 0 ? 0 : MaxCommission - overrideCommission;
+            }
+        }
+    }
+}
