9629c7e08f742f02825d8c68d5d40972470d05e7
Ian Baker <inbox@ianbaker.me>
2021-02-21 22:16:44 +0000

    Collection tests added

---
+++ PremiumCalculation.Tests/CollectionTests.cs
@@ -0,0 +1,53 @@
+﻿using PremiumCalculation.Models;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace PremiumCalculation.Tests
+{
+    public class CollectionTests
+    {
+        private List<Group> Groups = new List<Group>();
+
+        public CollectionTests()
+        {
+            this.Groups.Add(new Group()
+            {
+                Id = "Group1",
+                Name = "Group1Name",
+                Users = new List<User>() { new User() { Id = "User1", Name = "User1Name" }, new User() { Id = "User2", Name = "User2Name" } }
+            });
+            
+            this.Groups.Add(new Group()
+            {
+                Id = "Group2",
+                Name = "Group2Name",
+                Users = new List<User>() { new User() { Id = "User1", Name = "User1Name" }, new User() { Id = "User3", Name = "User3Name" } }
+            });
+        }
+
+        [Fact]
+        public void CheckSingleGroup()
+        {
+            // Act
+            var sut = this.Groups.FindAll(g => g.Id == "Group1");
+
+            // Assert
+            sut.Should().HaveCount(1);
+        }
+
+        [Theory]
+        [InlineData("User1", 2)]
+        [InlineData("User2", 1)]
+        [InlineData("User3", 1)]
+        [InlineData("Userx", 0)]
+        public void CheckSingleUser(string userId, int count)
+        {
+            // Act
+            var sut = this.Groups.FindAll(g => g.Users.Exists(u => u.Id == userId));
+
+            // Assert
+            sut.Should().HaveCount(count);
+        }
+    }
+}
---
+++ PremiumCalculation/Models/Group.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace PremiumCalculation.Models
+{
+    public class Group
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<User> Users { get; set; }
+    }
+}
